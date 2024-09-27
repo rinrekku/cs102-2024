255dba7c5e1b4ad9d6189e72e898888efa4fd50e
Dmitry Venin <dmvenin@gmail.com>
2024-09-27 19:17:42 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,6 +1,6 @@
 """Standard function provided by the university"""
 
 
-def text(message):
-    """Return a default message"""
-    return message
+def text():
+    """Return a predetermined string"""
+    return "message"
